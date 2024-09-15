3db8773a20a39db91a901e4329f45f61c4c43d1d
Kishan-kumar001 <kishan5183751@gmail.com>
2024-09-15 08:12:24 +0530

    sec commit

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("server is working...");
+import * as dotenv from "dotenv";
+import express from "express";
+const app = express();
+import morgan from "morgan";
+
+dotenv.config();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}...`);
+});
